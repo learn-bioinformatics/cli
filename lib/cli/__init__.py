72929cc18e54a14f765b6f2fea060246cdbf1747
Christopher Bottoms, PhD <Christopher-Bottoms@omrf.org>
2023-04-21 23:09:19 +0000

    Updated version number

--- lib/cli/__init__.py
+++ lib/cli/__init__.py
@@ -16,7 +16,7 @@ interface.
 # pragma: no cover
 
 __project__ = "pyCLI"
-__version__ = "devel"
+__version__ = "2.0.3.1"
 __package__ = "cli"
 __description__ = "Simple, object-oriented approach to Python CLI apps"
 __author__ = "Will Maier"
